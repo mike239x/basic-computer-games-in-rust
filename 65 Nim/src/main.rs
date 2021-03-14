340ab9e46cd9106fedabc5e482285c23664f299f
Mike Lezhnin <mikelezhnin@gmail.com>
2021-03-14 18:57:48 +0100

    Finish 65 NIM
    The code greatly differs from the original - improved both game logic
    as well as machine moves - original doesn't use xor and has special
    cases for only two piles; making the code more confusing than it needs
    to be.

--- 65 Nim/src/main.rs
+++ 65 Nim/src/main.rs
@@ -1,7 +1,8 @@
 use std::io;
+use rand::Rng;
 
 fn main() -> io::Result<()> {
-
+    let mut rng = rand::thread_rng(); // an rng for random moves
     println!("{}{}", " ".repeat(33), "NIM");
     println!("{}{}", " ".repeat(15), "CREATIVE COMPUTING  MORRISTOWN, NEW JERSEY");
     println!();
@@ -35,11 +36,12 @@ fn main() -> io::Result<()> {
             println!("PLEASE ANSWER YES OR NO");
         }
     }
+
     let mut running = true;
     while running {
-        let mut take_last: bool;
+        let take_last: bool;
         let mut move_first: bool;
-        let mut piles_count: usize;
+        let piles_count: usize;
         let mut pile_sizes: Vec::<i32>;
         println!();
         loop {
@@ -62,7 +64,7 @@ fn main() -> io::Result<()> {
                 }
             }
         }
-        pile_sizes = vec![0, piles_count as i32];
+        pile_sizes = vec![0; piles_count];
         println!("ENTER PILE SIZES");
         for i in 0..piles_count {
             loop {
@@ -91,25 +93,39 @@ fn main() -> io::Result<()> {
                 println!("PLEASE ANSWER YES OR NO.");
             }
         }
+
         // the game logic itself
         loop {
             if move_first {
+                println!("PILE SIZE");
+                for i in 0..piles_count {
+                    println!("{} {}", i, pile_sizes[i]);
+                }
+                // player to move
                 loop {
                     println!("YOUR MOVE - PILE, NUMBER TO BE REMOVED");
                     let mut input = String::new();
                     io::stdin().read_line(&mut input)?;
-                    //TODO parse two numbers separated by something (comma? space?)
-                    if let Ok(n) = input.trim().parse::<i32>() {
-                        //TODO
-                        break;
+                    // parse two numbers separated by a space
+                    let mut parts = input.trim().split(' ').map(|s| s.parse::<i32>());
+                    match (parts.next(), parts.next()) {
+                        (Some(Ok(a)), Some(Ok(b))) => {
+                            if a >= 0 && (a as usize) < piles_count && b > 0 {
+                                if b <= pile_sizes[a as usize] {
+                                    pile_sizes[a as usize] -= b;
+                                    break;
+                                }
+                            }
+                        },
+                        _ => {},
                     }
                 }
-                // if all_piles_empty(pile_sizes) {
-                //     println!("MACHINE LOSES");
-                //     break;
-                // }
             }
             move_first = true; // starting from the next loop player has to move
+            println!("PILE SIZE");
+            for i in 0..piles_count {
+                println!("{} {}", i, pile_sizes[i]);
+            }
 
             // the usual nim strategy is to keep xor of all pile sizes equal to zero
             // however there is one exception - if one is playing 'a misère game',
@@ -134,27 +150,59 @@ fn main() -> io::Result<()> {
             }
             match nonempty_piles_count - one_stone_piles_count {
                 0 => {
-                    if (one_stone_piles_count % 2 == 0) == take_last {
-                        // this covers even number of piles + taking last
-                        // and odd number of piles + leaving last
+                    if (one_stone_piles_count % 2 == 0) != take_last {
+                        // this covers even number of piles + machine taking last
+                        // and odd number of piles + player taking last
                         println!("MACHINE WINS");
                     } else {
                         println!("MACHINE LOSES");
                     }
-                    // TODO
                     break;
                 },
                 1 => {
                     // game is 1 move away from being over, depending on the goal either take the
                     // entire large pile or leave one stone in it
-                    // TODO
+                    for i in 0..piles_count {
+                        if pile_sizes[i] > 1 {
+                            if (one_stone_piles_count % 2 == 0) == take_last {
+                                pile_sizes[i] = 0;
+                            } else {
+                                pile_sizes[i] = 1;
+                            }
+                            break;
+                        }
+                    }
+                    println!("PILE SIZE");
+                    for i in 0..piles_count {
+                        println!("{} {}", i, pile_sizes[i]);
+                    }
+                    println!("MACHINE WINS");
+                    break;
                 },
                 _ => {
                     // game goes on, play to maintain the invariant
-                    // TODO
+                    let xor = pile_sizes.iter().fold(0i32, |acc, x| acc ^ x);
+                    if xor != 0 {
+                        // to find the right pile to take from just try them all
+                        for i in 0..piles_count {
+                            if pile_sizes[i] ^ xor < pile_sizes[i] {
+                                pile_sizes[i] ^= xor;
+                                break;
+                            }
+                        }
+                    } else {
+                        // do a random move
+                        loop {
+                            let i = rng.gen_range(0..piles_count);
+                            if pile_sizes[i] > 0 {
+                                let x = rng.gen_range(0..pile_sizes[i])+1;
+                                pile_sizes[i] -= x;
+                                break;
+                            }
+                        }
+                    }
                 },
             }
-            
         }
 
         println!("do you want to play another game");
@@ -173,119 +221,3 @@ fn main() -> io::Result<()> {
     }
     Ok(())
 }
-
-
-// 210 DIM A(100),B(100,10),D(2)
-// take_last - W == '1'
-// piles_count - N
-// pile_sizes[i] - A(I)
-// move_first - Q9$
-//
-//
-// 620 PRINT "DO YOU WANT TO MOVE FIRST";
-// 630 INPUT Q9$
-// 640 IF Q9$="YES" THEN 1450
-// 650 IF Q9$="yes" THEN 1450
-// 660 IF Q9$="NO" THEN 700
-// 670 IF Q9$="no" THEN 700
-// 680 PRINT "PLEASE ANSWER YES OR NO."
-// 690 GOTO 630
-//
-// 700 IF W=1 THEN 940
-// 710 LET C=0
-// 720 FOR I=1 TO N
-// 730 IF A(I)=0 THEN 770
-// 740 LET C=C+1
-// 750 IF C=3 THEN 840
-// 760 LET D(C)=I
-// 770 NEXT I
-// 780 IF C=2 THEN 920
-// 790 IF A(D(1))>1 THEN 820
-// 800 PRINT "MACHINE LOSES"
-// 810 GOTO 1640
-// 820 PRINT "MACHINE WINS"
-// 830 GOTO 1640
-// 840 LET C=0
-// 850 FOR I=1 TO N
-// 860 IF A(I)>1 THEN 940
-// 870 IF A(I)=0 THEN 890
-// 880 LET C=C+1
-// 890 NEXT I
-// 900 IF C/2<>INT(C/2) THEN 800
-// 910 GOTO 940
-// 920 IF A(D(1))=1 THEN 820
-// 930 IF A(D(2))=1 THEN 820
-// 940 FOR I=1 TO N
-// 950 LET E=A(I)
-// 960 FOR J=0 TO 10
-// 970 LET F=E/2
-// 980 LET B(I,J)=2*(F-INT(F))
-// 990 LET E=INT(F)
-// 1000 NEXT J
-// 1010 NEXT I
-// 1020 FOR J=10 TO 0 STEP -1
-// 1030 LET C=0
-// 1040 LET H=0
-// 1050 FOR I=1 TO N
-// 1060 IF B(I,J)=0 THEN 1110
-// 1070 LET C=C+1
-// 1080 IF A(I)<=H THEN 1110
-// 1090 LET H=A(I)
-// 1100 LET G=I
-// 1110 NEXT I
-// 1120 IF C/2<>INT(C/2) THEN 1190
-// 1130 NEXT J
-// 1140 LET E=INT(N*RND(1)+1)
-// 1150 IF A(E)=0 THEN 1140
-// 1160 LET F=INT(A(E)*RND(1)+1)
-// 1170 LET A(E)=A(E)-F
-// 1180 GOTO 1380
-// 1190 LET A(G)=0
-// 1200 FOR J=0 TO 10
-// 1210 LET B(G,J)=0
-// 1220 LET C=0
-// 1230 FOR I=1 TO N
-// 1240 IF B(I,J)=0 THEN 1260
-// 1250 LET C=C+1
-// 1260 NEXT I
-// 1270 LET A(G)=A(G)+2*(C/2-INT(C/2))*2^J
-// 1280 NEXT J
-// 1290 IF W=1 THEN 1380
-// 1300 LET C=0
-// 1310 FOR I=1 TO N
-// 1320 IF A(I)>1 THEN 1380
-// 1330 IF A(I)=0 THEN 1350
-// 1340 LET C=C+1
-// 1350 NEXT I
-// 1360 IF C/2<>INT(C/2) THEN 1380
-// 1370 LET A(G)=1-A(G)
-// 1380 PRINT "PILE  SIZE"
-// 1390 FOR I=1 TO N
-// 1400 PRINT I;A(I)
-// 1410 NEXT I
-//
-// 1420 IF W=2 THEN 1450
-// 1430 GOSUB 1570
-// 1440 IF Z=1 THEN 820
-// 1450 PRINT "YOUR MOVE - PILE, NUMBER TO BE REMOVED";
-// 1460 INPUT X,Y
-// 1470 IF X>N THEN 1450
-// 1480 IF X<1 THEN 1450
-// 1490 IF X<>INT(X) THEN 1450
-// 1500 IF Y>A(X) THEN 1450
-// 1510 IF Y<1 THEN 1450
-// 1520 IF Y<>INT(Y) THEN 1450
-// 1530 LET A(X)=A(X)-Y
-// 1540 GOSUB 1570
-// 1550 IF Z=1 THEN 800
-// 1560 GOTO 700
-// 1570 LET Z=0
-// 1580 FOR I=1 TO N
-// 1590 IF A(I)=0 THEN 1610
-// 1600 RETURN
-// 1610 NEXT I
-// 1620 LET Z=1
-// 1630 RETURN
-// 1720 GOTO 440
-
-// 1730 END
